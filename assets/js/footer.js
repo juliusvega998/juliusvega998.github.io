49600cebfb55ea7d2b60358e1c801b77aaa2d732
kintetsu998 <juliusvega998@gmail.com>
2016-12-13 17:22:09 +0800

    Added headers and footers

---
+++ assets/js/footer.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('footer').append(
+		'<p> Made by Julius Jireh B. Vega. &copy; 2016. </p>'
+	);
+});
---
+++ assets/js/header.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	$('header').append(
+		'<div id=\'banner-wrapper\'>' +
+			'<div class=\'row\'>' +
+				'<div class=\'col-sm-12\'>' +
+					'<img src=\'assets/img/banner.png\' id=\'banner\'/>' +
+				'</div>' +
+			'</div>' +
+		'</div>' +
+		'<nav class=\'navbar navbar-light\' id=\'navbar\'>' +
+			'<ul class=\'nav navbar-nav\'>' +
+				'<li class=\'nav-item active\'>' +
+					'<a class=\'nav-link\' href=\'#\'>About myself</a>' +
+				'</li>' +
+				'<li class=\'nav-item\'>' +
+					'<a class=\'nav-link\' href=\'#\'>Projects</a>' +
+				'</li>' +
+				'<li class=\'nav-item\'>' +
+					'<a class=\'nav-link\' href=\'#\'>Microsites</a>' +
+				'</li>' +
+				'<li class=\'nav-item\'>' +
+					'<a class=\'nav-link\' href=\'#\'>Contact me</a>' +
+				'</li>' +
+			'</ul>' +
+		'</nav>'
+	);
+});
+
+function activeNav(ctr) {
+	$(document).ready(function() {
+		console.log($('nav>ul>li:nth-child(' + ctr + ')>a').addClass('active'));
+	});
+}
