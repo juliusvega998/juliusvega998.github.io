9bdf1bf8204a81277d73db2cb4553eef4bee6cb3
kintetsu998 <juliusvega998@gmail.com>
2016-12-16 22:02:04 +0800

    Changed .html to .page

--- assets/js/router.js
+++ assets/js/router.js
@@ -4,32 +4,28 @@ function activeNav(hash) {
 
 	switch(hash) {
 		case '#about': 
-			document.title = 'Julius Vega | about_myself.html';
+			document.title = 'Julius Vega | about_myself.page';
 			$('section').load('templates/about.html', fadeLoader);
 			break;
-		case '#projects': 
-			document.title = 'Julius Vega | past_projects.html';
-			$('section').load('templates/projects.html', fadeLoader);
-			break;
 		case '#contact': 
-			document.title = 'Julius Vega | contact_me.html';
+			document.title = 'Julius Vega | contact_me.page';
 			$('section').load('templates/contact.html', fadeLoader);
 			break;
 		case '#my_picture.jpg':
 			document.title = 'Julius Vega | my_picture.jpg';
-			$('section').load('templates/my_picture.html', fadeLoader);
+			$('section').load('templates/pictures/my_picture.html', fadeLoader);
 			break;
 		case '#diet.png':
 			document.title = 'Julius Vega | diet.png';
-			$('section').load('templates/diet.html', fadeLoader);
+			$('section').load('templates/pictures/diet.html', fadeLoader);
 			break;
 		case '#nine.png':
 			document.title = 'Julius Vega | nine.png';
-			$('section').load('templates/nine.html', fadeLoader);
+			$('section').load('templates/pictures/nine.html', fadeLoader);
 			break;
 		case '#s1reborn.png':
 			document.title = 'Julius Vega | s1reborn.png';
-			$('section').load('templates/s1reborn.html', fadeLoader);
+			$('section').load('templates/pictures/s1reborn.html', fadeLoader);
 			break;
 		default: window.location.href = '404.html';
 	}
