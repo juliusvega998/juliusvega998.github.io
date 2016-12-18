c3848a93309641d469fce8e4e99ecb5e0e219db0
kintetsu998 <juliusvega998@gmail.com>
2016-12-18 22:42:11 +0800

    Changed section to main

--- assets/js/router.js
+++ assets/js/router.js
@@ -5,39 +5,39 @@ function activeNav(hash) {
 	switch(hash) {
 		case '#about': 
 			document.title = 'Julius Vega | about_myself.page';
-			$('section').load('templates/about.html', fadeLoader);
+			$('main').load('templates/about.html', fadeLoader);
 			break;
 		case '#contact': 
 			document.title = 'Julius Vega | contact_me.page';
-			$('section').load('templates/contact.html', fadeLoader);
+			$('main').load('templates/contact.html', fadeLoader);
 			break;
 		case '#my_picture.jpg':
 			document.title = 'Julius Vega | my_picture.jpg';
-			$('section').load('templates/pictures/my_picture.html', fadeLoader);
+			$('main').load('templates/pictures/my_picture.html', fadeLoader);
 			break;
 		case '#diet':
 			document.title = 'Julius Vega | diet_optimizer.page';
-			$('section').load('templates/projects/diet.html', fadeLoader);
+			$('main').load('templates/projects/diet.html', fadeLoader);
 			break;
 		case '#nine':
 			document.title = 'Julius Vega | project_nine.page';
-			$('section').load('templates/projects/nine.html', fadeLoader);
+			$('main').load('templates/projects/nine.html', fadeLoader);
 			break;
 		case '#s1reborn':
 			document.title = 'Julius Vega | systemone_reborn.page';
-			$('section').load('templates/projects/s1reborn.html', fadeLoader);
+			$('main').load('templates/projects/s1reborn.html', fadeLoader);
 			break;
 		case '#diet.png':
 			document.title = 'Julius Vega | diet.png';
-			$('section').load('templates/pictures/diet.html', fadeLoader);
+			$('main').load('templates/pictures/diet.html', fadeLoader);
 			break;
 		case '#nine.png':
 			document.title = 'Julius Vega | nine.png';
-			$('section').load('templates/pictures/nine.html', fadeLoader);
+			$('main').load('templates/pictures/nine.html', fadeLoader);
 			break;
 		case '#s1reborn.png':
 			document.title = 'Julius Vega | s1reborn.png';
-			$('section').load('templates/pictures/s1reborn.html', fadeLoader);
+			$('main').load('templates/pictures/s1reborn.html', fadeLoader);
 			break;
 		default: window.location.href = '404.html';
 	}
