20ad2f4de174d093bd1276b11fdae476cc850d3f
juliusvega998 <juliusvega998@gmail.com>
2017-06-11 19:52:17 +0800

    Improved loading animations. Refactored router.js.

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,59 +1,54 @@
 function activeNav(hash) {
-	const name = 'Julius Vega | '
+	const name = 'Julius Vega | ';
 	$('a').css('text-decoration', '');
 	$('a[href=\'' + hash +'\']').css('text-decoration', 'underline');
+	$(".button-collapse").sideNav('hide');
 
 	switch(hash) {
 		case '#about': 
-			document.title = name + 'about_myself.page';
-			$('main').load('templates/about.html', fadeLoader);
+			loadPage('templates/about.html', name + 'about_myself.page');
 			break;
 		case '#contact': 
-			document.title = name + 'contact_me.page';
-			$('main').load('templates/contact.html', fadeLoader);
+			loadPage('templates/contact.html', name + 'contact_me.page');
 			break;
 		case '#my_picture.jpg':
-			document.title = name + 'my_picture.jpg';
-			$('main').load('templates/pictures/my_picture.html', fadeLoader);
+			loadPage('templates/pictures/my_picture.html', name + 'my_picture.jpg');
 			break;
 		case '#diet':
-			document.title = name + 'diet_optimizer.page';
-			$('main').load('templates/projects/diet.html', fadeLoader);
+			loadPage('templates/projects/diet.html', name + 'diet_optimizer.page');
 			break;
 		case '#nine':
-			document.title = name + 'project_nine.page';
-			$('main').load('templates/projects/nine.html', fadeLoader);
+			loadPage('templates/projects/nine.html', name + 'project_nine.page');
 			break;
 		case '#s1reborn':
-			document.title = name + 'systemone_reborn.page';
-			$('main').load('templates/projects/s1reborn.html', fadeLoader);
+			loadPage('templates/projects/s1reborn.html', name + 'systemone_reborn.page');
 			break;
 		case '#twitch_annotator':
-			document.title = name + 'twitch_annotator.page';
-			$('main').load('templates/projects/sp.html', fadeLoader);
+			loadPage('templates/projects/sp.html', name + 'twitch_annotator.page');
 			break;
 		case '#diet.png':
-			document.title = name + 'diet.png';
-			$('main').load('templates/pictures/diet.html', fadeLoader);
+			loadPage('templates/pictures/diet.html', name + 'diet.png');
 			break;
 		case '#nine.png':
-			document.title = name + 'nine.png';
-			$('main').load('templates/pictures/nine.html', fadeLoader);
+			loadPage('templates/pictures/nine.html', name + 'nine.png');
 			break;
 		case '#s1reborn.png':
-			document.title = name + 's1reborn.png';
-			$('main').load('templates/pictures/s1reborn.html', fadeLoader);
+			loadPage('templates/pictures/s1reborn.html', name + 's1reborn.png');
 			break;
 		case '#twitch_annotator.png':
-			document.title = name + 'twitch_annotator.png';
-			$('main').load('templates/pictures/sp.html', fadeLoader);
+			loadPage('templates/pictures/sp.html', name + 'twitch_annotator.png');
 			break;
 		case '#': break;
 		default: window.location.href = '404.html';
 	}
 }
 
-function fadeLoader() {
-	$('div#loading').fadeOut();
-	putTabs();
+function loadPage(page, title) {
+	document.title = title;
+	$('main').fadeOut();
+	$('main').load(page, function() {
+		$('div#loading').fadeOut();
+		$('main').fadeIn();
+		putTabs();
+	});
 }
