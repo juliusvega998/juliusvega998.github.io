9c5a13974d3d0eabfaeecb1564d3fe9b705475d3
juliusvega998 <juliusvega998@gmail.com>
2017-06-11 18:44:01 +0800

    Added twitch annotator project to portfolio

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,43 +1,52 @@
 function activeNav(hash) {
+	const name = 'Julius Vega | '
 	$('a').css('text-decoration', '');
 	$('a[href=\'' + hash +'\']').css('text-decoration', 'underline');
 
 	switch(hash) {
 		case '#about': 
-			document.title = 'Julius Vega | about_myself.page';
+			document.title = name + 'about_myself.page';
 			$('main').load('templates/about.html', fadeLoader);
 			break;
 		case '#contact': 
-			document.title = 'Julius Vega | contact_me.page';
+			document.title = name + 'contact_me.page';
 			$('main').load('templates/contact.html', fadeLoader);
 			break;
 		case '#my_picture.jpg':
-			document.title = 'Julius Vega | my_picture.jpg';
+			document.title = name + 'my_picture.jpg';
 			$('main').load('templates/pictures/my_picture.html', fadeLoader);
 			break;
 		case '#diet':
-			document.title = 'Julius Vega | diet_optimizer.page';
+			document.title = name + 'diet_optimizer.page';
 			$('main').load('templates/projects/diet.html', fadeLoader);
 			break;
 		case '#nine':
-			document.title = 'Julius Vega | project_nine.page';
+			document.title = name + 'project_nine.page';
 			$('main').load('templates/projects/nine.html', fadeLoader);
 			break;
 		case '#s1reborn':
-			document.title = 'Julius Vega | systemone_reborn.page';
+			document.title = name + 'systemone_reborn.page';
 			$('main').load('templates/projects/s1reborn.html', fadeLoader);
 			break;
+		case '#twitch_annotator':
+			document.title = name + 'twitch_annotator.page';
+			$('main').load('templates/projects/sp.html', fadeLoader);
+			break;
 		case '#diet.png':
-			document.title = 'Julius Vega | diet.png';
+			document.title = name + 'diet.png';
 			$('main').load('templates/pictures/diet.html', fadeLoader);
 			break;
 		case '#nine.png':
-			document.title = 'Julius Vega | nine.png';
+			document.title = name + 'nine.png';
 			$('main').load('templates/pictures/nine.html', fadeLoader);
 			break;
 		case '#s1reborn.png':
-			document.title = 'Julius Vega | s1reborn.png';
+			document.title = name + 's1reborn.png';
 			$('main').load('templates/pictures/s1reborn.html', fadeLoader);
+			break;
+		case '#twitch_annotator.png':
+			document.title = name + 'twitch_annotator.png';
+			$('main').load('templates/pictures/sp.html', fadeLoader);
 			break;
 		case '#': break;
 		default: window.location.href = '404.html';
