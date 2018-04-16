fdb519809879177ea4db88dca3f35b226bd637f4
juliusvega998 <juliusvega998@gmail.com>
2018-04-16 15:16:51 +0800

    Added header mobile integration

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
-	$('header').load('templates/header.html', function() {
-		activeNav(window.location.hash);
-	});
+	if (navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i)) {
+		$('header').load('templates/m.header.html', function() {
+			activeNav(window.location.hash);
+		});	
+	} else {
+		$('header').load('templates/header.html', function() {
+			activeNav(window.location.hash);
+		});	
+	}
 });
