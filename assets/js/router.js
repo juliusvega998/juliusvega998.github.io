c9bd341847e73cdc3caa1c837244fac8f5bd81f3
juliusvega998 <juliusvega998@gmail.com>
2018-04-16 15:25:43 +0800

    Refactored router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -6,48 +6,56 @@ function activeNav(hash) {
 
 	switch(hash) {
 		case '#about': case '#': case '': 
-			loadPage('templates/about.html', name + 'about_myself.page');
+			loadPage('templates/', 'about.html', name + 'about_myself.page');
 			break;
 		case '#contact': 
-			loadPage('templates/contact.html', name + 'contact_me.page');
-			break;
-		case '#my_picture.jpg':
-			loadPage('templates/pictures/my_picture.html', name + 'my_picture.jpg');
+			loadPage('templates/', 'contact.html', name + 'contact_me.page');
 			break;
 		case '#diet':
-			loadPage('templates/projects/diet.html', name + 'diet_optimizer.page');
+			loadPage('templates/projects/', 'diet.html', name + 'diet_optimizer.page');
 			break;
 		case '#nine':
-			loadPage('templates/projects/nine.html', name + 'project_nine.page');
+			loadPage('templates/projects/', 'nine.html', name + 'project_nine.page');
 			break;
 		case '#s1reborn':
-			loadPage('templates/projects/s1reborn.html', name + 'systemone_reborn.page');
+			loadPage('templates/projects/', 's1reborn.html', name + 'systemone_reborn.page');
 			break;
 		case '#twitch_annotator':
-			loadPage('templates/projects/sp.html', name + 'twitch_annotator.page');
+			loadPage('templates/projects/', 'sp.html', name + 'twitch_annotator.page');
+			break;
+		case '#my_picture.jpg':
+			loadPage('templates/pictures/', 'my_picture.html', name + 'my_picture.jpg');
 			break;
 		case '#diet.png':
-			loadPage('templates/pictures/diet.html', name + 'diet.png');
+			loadPage('templates/pictures/', 'diet.html', name + 'diet.png');
 			break;
 		case '#nine.png':
-			loadPage('templates/pictures/nine.html', name + 'nine.png');
+			loadPage('templates/pictures/', 'nine.html', name + 'nine.png');
 			break;
 		case '#s1reborn.png':
-			loadPage('templates/pictures/s1reborn.html', name + 's1reborn.png');
+			loadPage('templates/pictures/', 's1reborn.html', name + 's1reborn.png');
 			break;
 		case '#twitch_annotator.png':
-			loadPage('templates/pictures/sp.html', name + 'twitch_annotator.png');
+			loadPage('templates/pictures/', 'sp.html', name + 'twitch_annotator.png');
 			break;
 		default: 
 			window.location.href = '404.html';
 	}
 }
 
-function loadPage(page, title) {
+function loadPage(prefix, page, title) {
 	document.title = title;
-	$('main').load(page, function() {
+	// if (isMobile() && page === 'templates/about.html') {
+	// 	page = 'templates/m.about.html'
+	// }
+
+	$('main').load(prefix + page, function() {
 		$('div#loading').fadeOut();
 		$('div#overlay').fadeOut();
 		putTabs();
 	});
+}
+
+function isMobile() {
+	return navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
 }
