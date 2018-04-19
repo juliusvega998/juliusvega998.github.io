046c50d01e9d2f8cf4d5fd2fc827cf51ac253163
juliusvega998 <juliusvega998@gmail.com>
2018-04-19 13:21:13 +0800

    Started converting to vanilla js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,3 +1,11 @@
-$(document).ready(function() {
-	$('footer').load('templates/footer.html');
-});
+// $(document).ready(function() {
+// 	$('footer').load('templates/footer.html');
+// });
+
+fetch("/templates/footer.html").then(function(response) {
+	return response.text();
+}).then(function(body) {
+	document.querySelector('footer').innerHTML = body;
+}).catch(function(err) {
+	alert(err);
+})
